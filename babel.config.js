ff0c338fba89d4172ff141ea70037ed32b3690d8
Iacopo <siuvdlec@gmail.com>
2022-05-26 09:39:43 +0200

    update

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 12 } }],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-
-  overrides: [
-    {
-      include: [
-        './packages/core',
-        './packages/one',
-        './packages/two',
-      ],
-      presets: [['@babel/preset-env', { targets: { esmodules: true } }]],
-    },
-  ],
-};
+    presets: [
+        ['@babel/preset-env', { targets: { node: 16, esmodules: true } }],
+        '@babel/preset-typescript',
+        '@babel/preset-react',
+    ],
+}
